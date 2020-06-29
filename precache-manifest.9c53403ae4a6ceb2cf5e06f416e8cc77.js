20661c2ef52dbf055858b744463d9e67d97c88c7
Adam Rybinski <adam@rybinski.info>
2020-06-29 04:21:54 +0200

    Updates

---
+++ precache-manifest.9c53403ae4a6ceb2cf5e06f416e8cc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f257bb16c1d246b905dbbd96f510fffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1120b2d069eb51407d6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9094a475d90d05cc6dd",
+    "url": "/static/js/2.b0b747d9.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/static/js/2.b0b747d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1120b2d069eb51407d6",
+    "url": "/static/js/main.db4116c4.chunk.js"
+  },
+  {
+    "revision": "6d4d1064af1be108afd6",
+    "url": "/static/js/runtime-main.0b8eacfd.js"
+  }
+]);
